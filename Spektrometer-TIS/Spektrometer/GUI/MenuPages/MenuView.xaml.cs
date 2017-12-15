10a6540cb63606948447e57d871aebab0ab86171
Radoslav Hečko <radohecko@users.noreply.github.com>
2017-12-15 18:35:54 +0100

    #41 CameraView layout

--- Spektrometer-TIS/Spektrometer/GUI/MenuPages/MenuView.xaml.cs
+++ Spektrometer-TIS/Spektrometer/GUI/MenuPages/MenuView.xaml.cs
@@ -24,5 +24,6 @@ namespace Spektrometer.GUI
         {
             InitializeComponent();
         }
+
     }
 }
--- Spektrometer-TIS/Spektrometer/MainWindow.xaml.cs
+++ Spektrometer-TIS/Spektrometer/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace Spektrometer
         public TopToolBar topToolBar { get; set; }
         public CameraRecordView cameraRecordViewer { get; set; }
         public SpektrometerService spektrometerService { get; set; }
+        // public delegate void NewPage(Page page)
 
         public MainWindow()
         {
