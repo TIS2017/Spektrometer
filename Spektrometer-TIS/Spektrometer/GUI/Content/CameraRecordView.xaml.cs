58e710a1e1fdeb6cc531402ff6c649e60dbd0962
Michal Singer <dareos1994@gmail.com>
2017-12-16 18:17:11 +0100

    #50 Zaciatok implementacie dedi od Canvas

---
+++ Spektrometer-TIS/Spektrometer/GUI/Content/CameraRecordView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spektrometer.GUI
+{
+    /// <summary>
+    /// Interaction logic for CameraRecordView.xaml
+    /// </summary>
+    
+    public partial class CameraRecordView : Canvas
+    {
+        public delegate void OnClick(int y);
+        public OnClick NewLineIndex { get; set; }
+        public CameraRecordView()
+        {
+            InitializeComponent();
+        }
+    }
+}
