f2fc846399041a6f24969f991992a28b4ffbff4b
Michal Plevka <michal.plevka@gmail.com>
2018-02-07 20:43:58 +0100

    Aktualizovanie GraphView

--- Spektrometer-TIS/Spektrometer/GUI/Content/GraphView.xaml.cs
+++ Spektrometer-TIS/Spektrometer/GUI/Content/GraphView.xaml.cs
@@ -43,6 +43,7 @@ namespace Spektrometer.GUI
         Boolean start = true;
 
         List<double> dataXcoords;
+        List<Color> pixelData; 
 
         public GraphView()
         {
@@ -53,7 +54,11 @@ namespace Spektrometer.GUI
 
         public void DrawGraph(List<Color> pixelData)
         {
-
+            if (pixelData == null)
+            {
+                return;
+            }
+            this.pixelData = pixelData;
             canGraph.Children.Clear();
 
             
@@ -68,9 +73,10 @@ namespace Spektrometer.GUI
             double lengthBetweenTwoAxisXPoints = 100;
             double lengthBetweenTwoAxisYPoints = 20;
             double countingAxisYPoints = 0;
+            double stepAxisY = (canGraph.Height - (2 * margin)) / 300;
 
             double counterForAxisX = lengthBetweenTwoAxisXPoints;
-            double counterForAxisY = ymax-lengthBetweenTwoAxisYPoints;
+            double counterForAxisY = ymax-(lengthBetweenTwoAxisYPoints*stepAxisY);
 
 
             if (start)
@@ -81,13 +87,15 @@ namespace Spektrometer.GUI
                 start = false;
             }
 
-            if (maxValueRange == pixelData.Count)
-            {
-                step = pixelData.Count / canGraph.Width;
-            } 
+
+            Console.Write("TEST");
+
+            //if (maxValueRange == pixelData.Count)
+            //{
+                step = ((maxValueRange - minValueRange) / (canGraph.Width - margin));
+            //} 
            
-            double stepForData = (canGraph.Width / (maxValueRange-minValueRange));
-            MessageBox.Show("GRAPH WIDTH: " + graphWidth);
+            //MessageBox.Show("GRAPH WIDTH: " + graphWidth);
 
             //MAXIMUM
             Line l = new Line();
@@ -113,60 +121,60 @@ namespace Spektrometer.GUI
                 PointCollection points = new PointCollection();
                 for (double x = minValueRange; x <= maxValueRange; x += step)
                 {
-                    if (count <= 1290)
+                    double yValue = 0;
+                    switch (data_set)
                     {
-                        double yValue = 0;
-                        switch (data_set)
-                        {
-                            case 0:
-                                yValue = ymax - pixelData.ElementAt((int)x).R;
-                                break;
-                            case 1:
-                                yValue = ymax - pixelData.ElementAt((int)x).G;
-                                break;
-                            case 2:
-                                yValue = ymax - pixelData.ElementAt((int)x).B;
-                                break;
-                            default:
-                                throw new Exception("");
+                        case 0:
+                            yValue = ymax - pixelData.ElementAt((int)x).R*stepAxisY;
+                            break;
+                        case 1:
+                            yValue = ymax - pixelData.ElementAt((int)x).G * stepAxisY;
+                            break;
+                        case 2:
+                            yValue = ymax - pixelData.ElementAt((int)x).B * stepAxisY;
+                            break;
+                        default:
+                            throw new Exception("");
 
-                        }
-                        points.Add(new Point(xmin + count, yValue));
-                        count += Math.Round(stepForData);
-
-                        if (data_set == 0)
-                        {
-                            dataXcoords.Add(xmin + count);
-                        }
                     }
+                    points.Add(new Point(xmin + count, yValue));
+                    count++;
+
+                    //if (data_set == 0)
+                    //{
+                    //    dataXcoords.Add(xmin + count);
+                    //}
                 }
-                MessageBox.Show("" + count);
+
+                //MessageBox.Show("" + count);
                 Polyline polyline = new Polyline();
                 polyline.StrokeThickness = 1;
                 polyline.Stroke = brushes[data_set];
                 polyline.Points = points;
                 canGraph.Children.Add(polyline);
-
+            }
 
             // X AXIS
             GeometryGroup xaxis_geom = new GeometryGroup();
             xaxis_geom.Children.Add(new LineGeometry(new Point(0, ymax), new Point(canGraph.Width, ymax))); //x-ova priamka
-            for (double x = xmin; x <= canGraph.Width; x += step)
+
+            double countAxisX = 0;
+            for (double x = minValueRange; x <= maxValueRange; x += step)
             {
                 if (x > counterForAxisX)
                 {
                     counterForAxisX += lengthBetweenTwoAxisXPoints;
                     xaxis_geom.Children.Add(new LineGeometry(
-                    new Point(x + margin, ymax - axisLineWidth / 2),
-                    new Point(x + margin, ymax + axisLineWidth / 2)));
+                    new Point(countAxisX + margin, ymax - axisLineWidth / 2),
+                    new Point(countAxisX + margin, ymax + axisLineWidth / 2)));
 
                     TextBlock txt = new TextBlock();
                     txt.Text = String.Format("{0:0}", counterForAxisX-100);
                     Canvas.SetTop(txt, ymax + 5);
-                    Canvas.SetLeft(txt, x + 20);
+                    Canvas.SetLeft(txt, countAxisX + 20);
                     canGraph.Children.Add(txt);
                 }
-
+                countAxisX += 1;
             }
             //MessageBox.Show(""+ countingAxisXPoints);
             Path xaxis_path = new Path();
@@ -179,11 +187,11 @@ namespace Spektrometer.GUI
             // Y AXIS
             GeometryGroup yaxis_geom = new GeometryGroup();
             yaxis_geom.Children.Add(new LineGeometry(new Point(xmin, 0), new Point(xmin, canGraph.Height)));
-            for (double y = ymax; y >= margin; y -= step)
+            for (double y = ymax; y >= ymin; y -= stepAxisY)
             {
-                if (y < counterForAxisY && countingAxisYPoints <= 12)
+                if (y < counterForAxisY)
                 {
-                    counterForAxisY -= lengthBetweenTwoAxisYPoints;
+                    counterForAxisY -= lengthBetweenTwoAxisYPoints*stepAxisY;
                     countingAxisYPoints++;
 
                     yaxis_geom.Children.Add(new LineGeometry(
@@ -192,7 +200,7 @@ namespace Spektrometer.GUI
 
                     TextBlock txt = new TextBlock();
                     txt.Text = String.Format("{0:0}", lengthBetweenTwoAxisYPoints*countingAxisYPoints);
-                    txt.Text = txt.Text.Substring(0, txt.Text.Length - 1) + "0";
+                    //txt.Text = txt.Text.Substring(0, txt.Text.Length - 1) + "0";
                     Canvas.SetTop(txt, y-10);
                     canGraph.Children.Add(txt);
                                         
@@ -211,9 +219,9 @@ namespace Spektrometer.GUI
 
 
 
-            }
+            
 
-            MessageBox.Show("STEP_DATA: " + stepForData + " X: " + initialX + " Y: " + finalX + " STEP: " + step + " WIDTH: " + canGraph.Width);
+            //MessageBox.Show("STEP_DATA: " + stepForData + " X: " + initialX + " Y: " + finalX + " STEP: " + step + " WIDTH: " + canGraph.Width);
 
         }
 
@@ -236,6 +244,11 @@ namespace Spektrometer.GUI
 
             step = 1;
             graphWidth = finalX - initialX;
+        }
+
+        private void canGraph_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawGraph(pixelData);
         }
     }
 }
